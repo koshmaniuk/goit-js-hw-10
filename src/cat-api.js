9804d81ac4b394a45ff5d4b5436a904b96e904b0
koshmaniuk <trident93@gmail.com>
2023-12-12 18:33:26 -0500

    final update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_sqb8Cv5y2YS9Hf2WEqoZ7KDBAEFJ7Xw9Xi8579d7Bims2AvGILCcxRFdBYsTGXic";
 
-const list = document.querySelector('.cat-info')
-const loader = document.querySelector('.loader')
-const errorText = document.querySelector('.error')
-
 function fetchBreeds() {
     return axios.get('https://api.thecatapi.com/v1/breeds')
         .then(resp => {
@@ -16,27 +12,14 @@ function fetchBreeds() {
 }
 
 function fetchCatByBreed(event) {
-    event.preventDefault()
     const breedId = event.target.value;
-    loader.style.display = "block";
-    list.innerHTML = '';
     return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
         .then(resp => {
-            loader.style.display = "none";
-            const catInfo = resp.data[0].breeds[0];
-            list.style.display = 'flex';
-            list.style.gap = '20px';
-            list.style.padding = '20px 0';
-            list.innerHTML = `<img src="${resp.data[0].url}" alt="${catInfo.name}" width='500' >
-                <div>
-                <h1>${catInfo.name}</h1>
-                <p>${catInfo.description}</p>
-                <p><span style="font-weight: bold;">Temperament:</span> ${catInfo.temperament}</p>
-                </div>`
+                return resp;
             })
             .catch(error => {
-                errorText.style.display = "block";
+                throw new Error(error)
             })
 }
-    
+
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_sqb8Cv5y2YS9Hf2WEqoZ7KDBAEFJ7Xw9Xi8579d7Bims2AvGILCcxRFdBYsTGXic";
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const select = document.querySelector('.breed-select')
 const loader = document.querySelector('.loader')
 const errorText = document.querySelector('.error')
+const list = document.querySelector('.cat-info')
 
 errorText.style.display = "none";
 
@@ -12,10 +11,14 @@ fetchBreeds()
     .then(data => {
         if(data.status === 200) {
             loader.style.display = "none";
+            errorText.style.display = "none";
             select.insertAdjacentHTML("beforeend", createMarkup(data.data));
         }
        })
-    .catch(error => console.log("Error!!!!", error))
+    .catch(error => {
+        errorText.style.display = "block";
+        console.log("Error:", error)
+    })
 
 function createMarkup(arr) {
     return arr.map(({ id, name }) => `
@@ -23,4 +26,27 @@ function createMarkup(arr) {
     `).join("")
 }
 
-select.addEventListener('change', fetchCatByBreed); 
+function createCatMarkup(event) {
+    event.preventDefault()
+    fetchCatByBreed(event)
+        .then(resp => {
+            errorText.style.display = "none";
+            loader.style.display = "none";
+            const catInfo = resp.data[0].breeds[0];
+            list.style.display = 'flex';
+            list.style.gap = '20px';
+            list.style.padding = '20px 0';
+            list.innerHTML = `<img src="${resp.data[0].url}" alt="${catInfo.name}" width='500' >
+                <div>
+                <h1>${catInfo.name}</h1>
+                <p>${catInfo.description}</p>
+                <p><span style="font-weight: bold;">Temperament:</span> ${catInfo.temperament}</p>
+                </div>`
+        })
+        .catch(error => {
+        errorText.style.display = "block";
+        console.log("Error:", error)
+        })
+}
+
+select.addEventListener('change', createCatMarkup)
